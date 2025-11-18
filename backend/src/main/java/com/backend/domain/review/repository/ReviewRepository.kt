527bf3c5951292d9ab9ba30c5d6daa751b7a7bfd
tobbot16 <tobbot16@naver.com>
2025-11-18 12:41:35 +0900

    feat: review, place 부분 추가할 사항 추가

--- backend/src/main/java/com/backend/domain/review/repository/ReviewRepository.kt
+++ backend/src/main/java/com/backend/domain/review/repository/ReviewRepository.kt
@@ -4,7 +4,6 @@ import com.backend.domain.review.entity.Review
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
-import java.util.*
 
 interface ReviewRepository : JpaRepository<Review, Long> {
     // 특정 회원이 특정 장소에 남긴 리뷰가 있는지 확인하는 메서드
@@ -14,4 +13,11 @@ interface ReviewRepository : JpaRepository<Review, Long> {
     fun findByMember_IdAndPlace_Id(memberId: Long, placeId: Long): Review?
     @Query("SELECT COALESCE(AVG(r.rating), 0) FROM Review r WHERE r.place.id = :placeId")
     fun findAverageRatingByPlaceId(@Param("placeId") placeId: Long?): Double
+
+    @Query("SELECT AVG(r.rating) FROM Review r")
+    fun findGlobalAverageRating(): Double
+
+
+    @Query("SELECT AVG(r.rating) FROM Review r WHERE r.place.id = :placeId")
+    fun findAverageRating(@Param("placeId") placeId: Long?): Double
 }
