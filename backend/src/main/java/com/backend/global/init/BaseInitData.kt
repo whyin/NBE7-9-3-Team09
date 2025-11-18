39c7f0d76acc4bdce1ff0ab6ab7c3e54f76130df
seongchan <otr995@gmail.com>
2025-11-18 17:41:08 +0900

    feat(bookmark): jpafactory 설정 파일 추가

--- backend/src/main/java/com/backend/global/init/BaseInitData.kt
+++ backend/src/main/java/com/backend/global/init/BaseInitData.kt
@@ -29,7 +29,6 @@ import org.springframework.boot.ApplicationRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
 import java.util.List
 
